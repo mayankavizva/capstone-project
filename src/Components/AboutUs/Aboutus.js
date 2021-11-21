0015c312f4957151a61c5f5ae734b6fab0c25cae
ashish <root@Ashish-Singh-MBP.local>
2021-11-21 15:25:30 +0530

    aboutus and misson page added

---
+++ src/Components/AboutUs/Aboutus.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Aboutus = () => {
+    return (
+        <div>
+            <h1>Who we are</h1>
+            <p>Team of people who risk, learn, and grow together, in the name of progress. We come to work each day looking at everything through the lens of "How can we make this better?" Easyinsure has helped individual and institutional customers grow and protect their wealth. We are known for delivering on our promises to our customers, and are recognized as one of the most admired companies. </p>
+            <h1>What we do</h1>
+            <p>We provide customers with a variety of products and services, including automobile insurance, life insurance, annuities, retirement-related services, mutual funds and investment management. We strive to create long-term value for our stakeholders through strong business fundamentals, consistent with our mission guided by our vision and directed by our company's core values.</p>
+            
+        </div>
+    )
+}
